0689a23287baa6d1a88071ee018b53da32bbd9c7
ZugnuKhatoon07 <02zugnu2005@gmail.com>
2024-11-24 18:58:22 +0530

    conversion casting and or not

--- day1/Hello.py
+++ day1/Hello.py
@@ -29,9 +29,65 @@ a = None
 print(type(old))
 print(type(a))
 
+
+
+#arithmetic oprators
 a=2
 b=4
 sum=a+b
+#print(sum)
+print(a+b)
+print(a/b)
+print(a**b)
+print(a%b)
+print(a*b)
+print(a-b)
+
+#relation operator
+a=50
+b=20
+print(a==b) #False
+print(a!=b) #True
+print(a>=b) #True
+print(a>b) #True
+
+#assignment operators
+num = 10
+num -= 10
+num += 10
+num *= 10
+num /= 10
+num %= 10
+num **= 10
+a
+print("num :", num)
+
+#logical operators
+a=50
+b=30
+
+print(not False)
+print(not (a>b))
+
+val1 = True
+val2 = False
+print("and operator:",val1 and val2)
+print("or operator:",val1 or val2)
+print("or operator:",(a == b) or (a>b))
+
+#type conversion
+a = 2
+b = 4.25
+sum = a +b #2.0 + 4.25 => 6.25
 print(sum)
+
+#Type casting
+a=2
+b=float(4.25)
+print(type(b))
+print(a+b)
+
+name=input("enter any name:")
+print("welcome", name)
 
 
