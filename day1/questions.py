3f8aa1c34dc87e7018c07e3f9ef6cd791375f5e1
ZugnuKhatoon07 <02zugnu2005@gmail.com>
2024-11-25 12:43:03 +0530

    pactic question

---
+++ day1/questions.py
@@ -0,0 +1,57 @@
+#__________________***what is the different between // and % in python?***__________________
+# a=10
+# b=5
+# print(a%b)
+
+
+
+#__________________***write a program to calulate the product and sum of two number.***__________________
+# a=40
+# b=60
+# print(a+b)
+
+
+
+#__________________***For a = 10 and b = 20, what will be the output of these .***__________________
+# a = 10
+# b = 20
+# print(a < b , a > b , a==b ,a!=b)
+
+
+
+#__________________***write a program to check if a number is odd or even***__________________
+# number=int(input("enter any number" ))
+# if number % 2 == 0 :
+#     print("nuber is even")
+# else:
+#     print("number is odd")
+
+
+
+#__________________***if x = True and y=false , what will be output  if?***__________________
+# x = 20
+# y = 30
+# print(x and y, x or y, not y)
+
+
+
+#__________________***write a program to check if a numbre entered by the user lies btween 10 and 50?***__________________
+# num = int(input("enter any number between 10 and 50 : " ))
+# if num >= 10 and num <=50 :
+#     print("number lies ")
+# else:
+#     print("number not lies")
+
+
+#For a = 5 , what will be the outout the folling?
+# a = 5
+# a += 3
+# print(a)
+# a *= 2
+# print(a)
+
+
+#if a = 5 and b = 3 what will be the uotput of?
+a = 5
+b = 3
+print(a & b )
