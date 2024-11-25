b9d87d4c1c3b97d681148e9b9feafbc7d8392417
ZugnuKhatoon07 <02zugnu2005@gmail.com>
2024-11-25 19:38:16 +0530

    pactis questions

--- day1/questions.py
+++ day1/questions.py
@@ -34,8 +34,6 @@
 # print(x and y, x or y, not y)
 
 
-
-#__________________***write a program to check if a numbre entered by the user lies btween 10 and 50?***__________________
 # num = int(input("enter any number between 10 and 50 : " ))
 # if num >= 10 and num <=50 :
 #     print("number lies ")
@@ -43,7 +41,7 @@
 #     print("number not lies")
 
 
-#For a = 5 , what will be the outout the folling?
+#_________________***For a = 5 , what will be the outout the folling?***__________________
 # a = 5
 # a += 3
 # print(a)
@@ -51,7 +49,37 @@
 # print(a)
 
 
-#if a = 5 and b = 3 what will be the uotput of?
-a = 5
-b = 3
-print(a & b )
+#_________________***if a = 5 and b = 3 what will be the uotput of?***__________________
+# a = 5
+# b = 3
+# print(a & b )
+
+# #_________________***writ a program to input 2 number & print their sum?***__________________
+# a = 34
+# b = 56
+# print(a + b)
+
+# frist = int(input("enter first : "))
+# second = int(input("enter any number : "))
+# print("sum = ", frist + second)
+
+#_________________***WAP to input side of a square & print its area.***__________________
+# side = float(input("enter any number side square : "))
+# area=side*side
+# print("side square : ",area )
+
+#_________________***WAP to input 2 floating point numbers& print their average.***__________________
+# a = float(input("enter any number : "))
+# b = float(input("enter any number : "))
+# average = (a + b)/2
+# print(average)
+
+
+#WAP to input to input 2 int numbers, a and b.print true if a is greater than or equal to b . if not print false
+# a = int(input("enter any number :"))
+# b = int(input("enter any number :"))
+
+
+# print(a >= b)
+
+
