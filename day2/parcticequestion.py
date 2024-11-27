454739488a0cd3770c6e3b1b783c2d63c5fa8cb6
ZugnuKhatoon07 <02zugnu2005@gmail.com>
2024-11-27 22:33:32 +0530

    parcticequestion

---
+++ day2/parcticequestion.py
@@ -0,0 +1,13 @@
+#_________***let's Practice***_________
+
+#WAP to input user's first name & print its length.
+name = input("enter any name : ")
+# print(name)
+# print(len(name))
+print("length of your nsme is ", len(name))
+
+
+#WAP to find the occurrence of '$'in a String.
+str = "i am$ zugnu$ from siwan$","$99.99"
+print(str.count("$"))
+
