411c8703bada90bb9f298e01dec56ea1196a3d79
ZugnuKhatoon07 <02zugnu2005@gmail.com>
2024-11-28 20:10:35 +0530

    listMethods

---
+++ day3/listSlicing.py
@@ -0,0 +1,6 @@
+#______****lisr Slicing***__________
+marks = [87, 64, 33, 95, 76]
+print(marks[0 : 4])
+print(marks[ : 4])
+print(marks[0 : ])
+print(marks[0 : 5])
---
+++ day3/lists.py
@@ -0,0 +1,20 @@
+marks = [94.4, 87.5, 95.2, 66.4, 45.1]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(len(marks))
+
+student = ["zugnu", 90.15, "patna"]
+print(student)
+
+#____***string me assnid karna allowed nahi hai***_______
+str = "hello"
+print(str[0])
+str[0] = "y"
+
+student = ["zugnu", 90.15, "patna"]
+print(student[0])
+student[0] = "sarfaraz"
+print(student)
+
