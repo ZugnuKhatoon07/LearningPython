e2b89ece635f26ac73423d01be986b5a57267787
ZugnuKhatoon07 <02zugnu2005@gmail.com>
2024-12-12 23:47:05 +0530

    for lopp

--- day5/Practicequestion.py
+++ day5/Practicequestion.py
@@ -57,4 +57,93 @@ while i <= 5 :
         break
     i += 1 
     
+#______****Continue : terminates execution in the curren iteration & continues execution of the loop with the net iteration.***_________
+i = 0 
+while i < 5 :
+    if(i == 3):
+        i += 1
+        continue
+    print(i)
+    i += 1
 
+# _____***skip even***_________
+i = 0 
+while i < 10 :
+    if(i %2 == 0):
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+# ____________**** skip odd nomber *******________
+i = 0
+while i < 10 :
+    if(i %2 != 0):
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+#____________****for practiceuestion***_____________
+#print the element of the following list using a loop :
+nums = (1,4,9,16,25,36,49,64,81,100)
+x = 49
+
+idx = 0
+for val in nums :
+    if(val == x):
+        print("number found at idx",)
+    idx += 1
+
+
+#range(start?,stop,step?)
+for el in range(5):
+    print(el)
+for el in range(1,5):
+    print(el)
+for el in range(1,5,2):
+    print(el)
+
+
+#____________*****Forloop Practicequestion****____________
+#print numbers from 1 to 100.
+for i in range(1 , 101):
+    print(i)
+
+
+#___________****print numbers from 100 to 1.***_________
+for i in range(100,0,-1):
+    print(i)
+
+#__________***print the multiplication table of a number n.***___________
+n = int(input("enter any number :"))
+for i in range(1, 11):
+    print(n*i)
+
+#________***Pass Ststement***_________
+# pass is null ststement thst does nothing . it is used as a placeholder for future code.
+for i in range(5):
+    pass
+
+if i > 5:
+    pass
+
+print("some usefil work")
+
+#___________***WAP to find the sum of first n numbers . (using while)****________
+n = int(input("enter any number :"))
+sum = 0
+for i in range(1, n+1):
+
+    sum += i
+    i += 1
+print("total sum =",sum)
+
+#WAP to find the factorial of first n numbers 
+n = 5 
+fact = 1
+i = 1
+while i <= n :
+    fact *= i
+    i += 1
+print("factorial=", fact)
---
+++ day5/forloop.py
@@ -0,0 +1,24 @@
+#___***1***___
+nums = [1,2,3,4,5]
+for val in nums :
+    print(val)
+
+#___***2***
+veggies = ["potato","brijal","ladyfinger","cucumber"]
+for val in veggies :
+   print(val)
+
+#___***3***_____
+tup = (1,2,3,4,5,2)
+for num in tup :
+    print(num)
+
+#____***4***____   
+str = "zugnujaan"
+for char in str :
+    if(char == 'j'):
+        print("j found")
+        break
+    print(char)
+
+print("END")
